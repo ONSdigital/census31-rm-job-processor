435fe065d7c078ba63721b7dd4169bdb25a119b1
Ryan Grundy <ryan.grundy@ons.gov.uk>
2026-09-07 15:01:06 +0100

    CN-185 Update refusal topic/subscription

--- src/test/resources/setup_pubsub.sh
+++ src/test/resources/setup_pubsub.sh
@@ -6,8 +6,8 @@ bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' '$PUBSUB_SETUP_HOS
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/our-project/topics/event_new-case
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/our-project/subscriptions/event_new-case_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_new-case"}'
 
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/our-project/topics/event_refusal
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/our-project/subscriptions/event_refusal_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_refusal"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/our-project/topics/event_refusal-received
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/our-project/subscriptions/event_refusal-received_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_refusal-received"}'
 
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/our-project/topics/event_invalid-case
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/our-project/subscriptions/event_invalid-case_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_invalid-case"}'
